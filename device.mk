c8bbdf4b6d3c86d7d75c838d81d638aec944ca70
kurtnettle <89929240+KurtNettle@users.noreply.github.com>
2026-09-22 20:18:09 +0600

    m51: ofox: add health HAL 2.1 support for recovery
    Signed-off-by: kurtnettle <shafeen@duck.com>

--- device.mk
+++ device.mk
@@ -6,3 +6,9 @@
 #
 
 LOCAL_PATH := device/samsung/m51
+
+#Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
